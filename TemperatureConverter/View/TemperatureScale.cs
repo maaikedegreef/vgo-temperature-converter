64bb8ae5c2de723bc5e6748bd72ba1456cf6991e
Maaike De Greef <maaike.degreef@student.ucll.be>
2022-03-07 15:35:47 +0100

    Exercise 12
    Refactoring - In TemperatureScale.cs, define a class KelvinTemperatureScale that implements ITemperatureScale. Each conversion method receives a temperature in Kelvin and is supposed to convert it into Kelvin, so it's normal that both these methods are indeed that simple.

---
+++ TemperatureConverter/View/TemperatureScale.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View
+{
+    internal class TemperatureScale
+    {
+
+        public class KelvinTemperatureScale : ITemperatureScale
+        {
+            // shortened syntax for getter that consits of a sole return statement
+            public string Name => "Kelvin";
+
+            public double ConvertFromKelvin(double temperature)
+            {
+                return temperature;
+            }
+
+            public double ConvertToKelvin(double temperature)
+            {
+                return temperature;
+            }
+        }
+    }
+}
