dc7646ac6253cd315a9e3ee837efc079cec6f549
Maaike De Greef <maaike.degreef@student.ucll.be>
2022-03-13 10:41:40 +0100

    Exercise 17
    Refactoring - Create a new file ViewModel.cs.
    In this file, create a new class ConverterViewModel.
    Move all TemperatureInKelvin related code to this class. This includes the field, the actual property, the INotifyPropertyChanged interface.
    Remove the INotifyPropertyChanged functionality from the MainWindow class.
    In the MainWindow constructor, set the MainWindows DataContext to a new instance of ConverterViewModel.

---
+++ TemperatureConverter/View/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace View
+{
+    public class ConverterViewModel : INotifyPropertyChanged
+    {
+        private double temperatureInKelvin;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public double TemperatureInKelvin
+        {
+            get
+            {
+                return temperatureInKelvin;
+            }
+            set
+            {
+                temperatureInKelvin = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TemperatureInKelvin)));
+            }
+        }
+    }
+}
