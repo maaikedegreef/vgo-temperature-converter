8e726b03cf2c496020754812996203ba0fb2f437
Maaike De Greef <maaike.degreef@student.ucll.be>
2022-03-13 11:29:37 +0100

    Exercise 18
    Refactoring - Remove the TemperatureConverter class and resources.

--- TemperatureConverter/View/ViewModel.cs
+++ TemperatureConverter/View/ViewModel.cs
@@ -12,6 +12,12 @@ namespace View
         private double temperatureInKelvin;
 
         public event PropertyChangedEventHandler PropertyChanged;
+        public ConverterViewModel()
+        {
+            this.Kelvin = new TemperatureScaleViewModel(this, new KelvinTemperatureScale());
+            this.Celsius = new TemperatureScaleViewModel(this, new CelsiusTemperatureScale());
+            this.Fahrenheit = new TemperatureScaleViewModel(this, new FahrenheitTemperatureScale());
+        }
 
         public double TemperatureInKelvin
         {
@@ -25,5 +31,39 @@ namespace View
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TemperatureInKelvin)));
             }
         }
+
+        public TemperatureScaleViewModel Kelvin { get; }
+        public TemperatureScaleViewModel Celsius { get; }
+        public TemperatureScaleViewModel Fahrenheit { get; }
+    }
+
+    public class TemperatureScaleViewModel : INotifyPropertyChanged
+    {
+        private readonly ConverterViewModel parent;
+        private readonly ITemperatureScale temperatureScale;
+
+        public TemperatureScaleViewModel(ConverterViewModel parent, ITemperatureScale temperatureScale)
+        {
+            this.parent = parent;
+            this.temperatureScale = temperatureScale;
+            this.parent.PropertyChanged += (sender, args) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Temperature)));
+        }
+
+        public string Name => temperatureScale.Name;
+
+        public double Temperature
+        {
+            get
+            {
+                return temperatureScale.ConvertFromKelvin(parent.TemperatureInKelvin);
+            }
+            set
+            {
+                parent.TemperatureInKelvin = temperatureScale.ConvertToKelvin(value);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
     }
 }
