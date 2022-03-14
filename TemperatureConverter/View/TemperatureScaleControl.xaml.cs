af6dedd174a42595a2714ec17e2ff93fe23ac5fd
Maaike De Greef <maaike.degreef@student.ucll.be>
2022-03-14 16:18:37 +0100

    Exercise 31
    Reusable View Components - Create a second dependency property.
    
    Name it Value.
    Its type is double.
    Bind the text box to it.
    Update MainWindow.xaml

--- TemperatureConverter/View/TemperatureScaleControl.xaml.cs
+++ TemperatureConverter/View/TemperatureScaleControl.xaml.cs
@@ -33,5 +33,16 @@ namespace View
 
         public static readonly DependencyProperty HeaderProperty =
             DependencyProperty.Register("Header", typeof(string), typeof(TemperatureScaleControl), new PropertyMetadata(""));
+
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(double), typeof(TemperatureScaleControl), new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+
     }
 }
