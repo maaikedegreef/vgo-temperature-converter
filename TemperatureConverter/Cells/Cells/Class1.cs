cd61f51af223752ca052ffe5b6ffa36f20ca67a7
Maaike De Greef <maaike.degreef@student.ucll.be>
2022-03-14 11:13:38 +0100

    Exercise 22
    Cells - Create a new project named Cells.

---
+++ TemperatureConverter/Cells/Cells/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cells
+{
+    public class Cell : INotifyPropertyChanged
+    {
+        private object contents;
+
+        public Cell(object initialContents = null)
+        {
+            this.contents = initialContents;
+        }
+
+        public object Value
+        {
+            get
+            {
+                return contents;
+            }
+            set
+            {
+                this.contents = value;
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
